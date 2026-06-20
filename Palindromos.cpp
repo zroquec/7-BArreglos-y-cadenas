e2a6eff326d801473c602cf0b706676a8a15ad0d
zroquec <zroquec@unjbg.edu.pe>
2026-06-20 18:40:51 -0500

    EjercicioB: Primer avance de palindromos

---
+++ Palindromos.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool palindromo(string palabra);
+
+int main (){
+    int N;
+    string nombres[50];
+    cin>>N;
+  
+    for(int i = 0; i < N; i++){
+        cin>>nombres[i];
+    }
+    
+    for(int i = 0; i < N; i++){
+        if(palindromo(nombres[i])){
+            cout<<"P"<<endl;
+        }
+        else{
+            cout<<"NP"<< endl;
+        }
+    }
+    return 0;
+}
+
+bool palindromo(string palabra){
+
+}
