656d686c9d983fe9fae2cb8a785c4fac743c5cba
zroquec <zroquec@unjbg.edu.pe>
2026-06-21 11:41:59 -0500

    EjercicioC: Segundo intento de frecuencias

--- Frecuencias.cpp
+++ Frecuencias.cpp
@@ -2,30 +2,28 @@
 using namespace std;
 
 int main(){
-int P,n,f;
-int alumnos[1000];
-int conta[100];
-cin>>P;
-cin>>n;
-for (int i=0;i<P;i++){
-    conta[i]=0;
-}
-for(int i=0; i<n; i++){
-    cin>>alumnos[i];
-}
-for(int i=0;i<n; i++){
-    f=1;
-    while(f<5){
+    int P,n,f, alumnos[1000];
+    int conta[100]={0};
+    cin>>P;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>alumnos[i];
+    }
+    for(int i=0;i<n; i++){
+        f=1;
+        while(f<5){
         if(alumnos[i]!=f){  
-            f++;
         }
         else{
             conta[f-1]++;
         }
+        f++;
+        }
     }
-}
 
-for(int i=0;i<P;i++){
+    for(int i=0;i<P;i++){
     cout<<i+1<<"-"<<conta[i]<<'\n';
-}
+    }
+    return 0;
 }
