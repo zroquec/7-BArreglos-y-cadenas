6d9d3ede81339a5be8901b21a24711ad1bf932f4
zroquec <zroquec@unjbg.edu.pe>
2026-06-20 21:52:26 -0500

    EjercicioB: Terminado ejercicio de palindromo

--- Palindromos.cpp
+++ Palindromos.cpp
@@ -8,11 +8,10 @@ int main (){
     string nombres[50];
     cin>>N;
   
-    for(int i = 0; i < N; i++){
+    for(int i=0; i < N; i++){
         cin>>nombres[i];
     }
-    
-    for(int i = 0; i < N; i++){
+    for(int i=0; i < N; i++){
         if(palindromo(nombres[i])){
             cout<<"P"<<endl;
         }
@@ -24,5 +23,25 @@ int main (){
 }
 
 bool palindromo(string palabra){
+ int i=0;
+    int cont=0; 
+    int igual=0; 
 
+    while(palabra[i] != '\0'){
+        cont++;
+        i++;
+    }
+
+    for(int i=0; i<int(cont / 2); i++){
+        if(palabra[i]==palabra[cont-i-1]){
+            igual++;
+        }
+    }
+
+    if(igual==int(cont / 2)){
+        return true;
+    }
+    else {
+        return false; 
+    }
 }
